e2206c4d6570f50a1c7754790afe84b0284a6ef4
Ulises Monte <133891462+ulisesMonte@users.noreply.github.com>
2025-01-27 17:43:46 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
 )
 
 const db = createClient({
-    url:"libsql://balanced-dead-girl-ulisesmonte.turso.io",
+    url:"" // turso db,
     authToken:process.env.DB_TOKEN,
 })
 
